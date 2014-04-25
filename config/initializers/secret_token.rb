ac2e5d1b65bbbf75c4bf39fd24f1a5ac51a24205
saikiranmothe <saikiran.mothe@gmail.com>
2014-04-25 17:31:39 +0530

    these my new files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logicatet::Application.config.secret_token = '93be6076ecb0f65630275ab44576710bb0f2f67eb2155077e4de6467f477a080ca8428465b96e0ba0523b4a052caf93a6afb322fa24aff3060a7f758a99e1fb6'
